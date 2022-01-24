ed3dc5c11a1f22110bbd27b034762eec80fb8b8d
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-01-25 02:39:22 +0300

    Update Main.kt
    Refactored decimal conversion

--- Main.kt
+++ Main.kt
@@ -1,31 +1,26 @@
 package converter
 
 fun main() {
-    println("Enter number in decimal system:")
-    val number = readLine()!!.toInt()
-    println("Enter target base:")
-    val radix = readLine()!!.toInt()
-    println("Conversion result: ${convertDecimalToRadix(number, radix)}")
-}
-
-private fun convertDecimalToBinary(number: Int): String {
-    val listOfRemainders = mutableListOf<Int>()
-    var quotient = number
     do {
-        listOfRemainders.add(0,quotient % 2)
-        quotient /= 2
-    } while (quotient > 0)
-    return listOfRemainders.joinToString("")
-}
-
-private fun convertDecimalToOctal(number: Int): String {
-    val listOfRemainders = mutableListOf<Int>()
-    var quotient = number
-    do {
-        listOfRemainders.add(0,quotient % 8)
-        quotient /= 8
-    } while (quotient > 0)
-    return listOfRemainders.joinToString("")
+        println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)")
+        val input = readLine()!!
+        when (input) {
+            "from" -> {
+                println("Enter number in decimal system:")
+                val number = readLine()!!.toInt()
+                println("Enter target base:")
+                val radix = readLine()!!.toInt()
+                println("Conversion result: ${convertDecimalByTargetBase(number, radix)}")
+            }
+            "to" -> {
+                println("Enter source number:")
+                val number = readLine()!!.toInt()
+                println("Enter source base:")
+                val base = readLine()!!.toInt()
+                println("Conversion to decimal result: ${convertDecimalByTargetBase(number, base)}")
+            }
+        }
+    } while (input != "exit")
 }
 
 private fun matchDecimalToHexadecimal(number: Int): String {
@@ -40,20 +35,22 @@ private fun matchDecimalToHexadecimal(number: Int): String {
     }
 }
 
-private fun convertDecimalToHexadecimal(number: Int): String {
+private fun convertDecimal(number: Int, radix: Int): String {
     val listOfRemainders = mutableListOf<String>()
     var quotient = number
     do {
-        listOfRemainders.add(0, matchDecimalToHexadecimal(quotient % 16))
-        quotient /= 16
+        listOfRemainders.add(0,
+            if (radix != 16) "${quotient % radix}" else matchDecimalToHexadecimal(quotient % 16))
+        )
+        quotient /= radix
     } while (quotient > 0)
     return listOfRemainders.joinToString("")
 }
 
-private fun convertDecimalToRadix(number: Int, radix: Int): String {
+private fun convertDecimalByTargetBase(number: Int, radix: Int): String {
     return when (radix) {
-        8 -> convertDecimalToOctal(number)
-        2 -> convertDecimalToBinary(number)
-        else -> convertDecimalToHexadecimal(number)
+        8 -> convertDecimal(number, 8)
+        2 -> convertDecimal(number, 2)
+        else -> convertDecimal(number, 16)
     }
 }
